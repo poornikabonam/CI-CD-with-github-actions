07b3a173659224173bced2ebe5f99e8cfaa323b4
Poornika Bonam <pbonam3@gmail.com>
2024-10-05 23:17:29 -0400

    added flask code and workflows

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from app import app
+
+def test_hello_world():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
+
+# Run the test
+# $ pytest test_app.py
+# ============================= test session starts ==============================  
+
+
